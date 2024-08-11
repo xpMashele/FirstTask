49a45da17ef7f323f02a241d847bca680e39bded
1234 <1234@DESKTOP-ERO2I20>
2024-08-11 10:41:36 -0700

    First commit

---
+++ src/firstjavaprogramhomework/FirstJavaProgramhomework.java
@@ -0,0 +1,18 @@
+
+package firstjavaprogramhomework;
+
+/**
+ *Student Name: XITSHEMBISO PROMISE MASHELE
+ *Student Number: ST10445295
+ * Assignment Name: Homework 1
+ * Submission due date:26 July 2024
+ */
+public class FirstJavaProgramhomework {
+
+  
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello world!!");
+    }
+    
+}
